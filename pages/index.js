5701bf63b3d3adb8fc45bf40e6ff3f220c45a97e
Wonki Lee <95551598+Lee01key@users.noreply.github.com>
2022-03-27 15:59:14 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Project Portfolio: Wonki Lee</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Project 3" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
